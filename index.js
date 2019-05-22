c08472d49ad14550bc32dbf762651f3583204cac
acharl <amadeo@fermat.org>
2019-05-22 15:28:23 +0200

    fix(async): returns address instead of error

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const EventEmitter = require('events').EventEmitter
-const inherits = require('util').inherits
-const ethUtil = require('ethereumjs-util')
-const EthBlockTracker = require('eth-block-tracker')
-const map = require('async/map')
-const eachSeries = require('async/eachSeries')
-const Stoplight = require('./util/stoplight.js')
-const cacheUtils = require('./util/rpc-cache-utils.js')
-const createPayload = require('./util/create-payload.js')
-const noop = function(){}
+const EventEmitter = require("events").EventEmitter
+const inherits = require("util").inherits
+const ethUtil = require("ethereumjs-util")
+const EthBlockTracker = require("eth-block-tracker")
+const map = require("async/map")
+const eachSeries = require("async/eachSeries")
+const Stoplight = require("./util/stoplight.js")
+const cacheUtils = require("./util/rpc-cache-utils.js")
+const createPayload = require("./util/create-payload.js")
+const noop = function() {}
 
 module.exports = Web3ProviderEngine
-
 
 inherits(Web3ProviderEngine, EventEmitter)
 
@@ -24,15 +23,17 @@ function Web3ProviderEngine(opts) {
   // block polling
   const directProvider = { sendAsync: self._handleAsync.bind(self) }
   const blockTrackerProvider = opts.blockTrackerProvider || directProvider
-  self._blockTracker = opts.blockTracker || new EthBlockTracker({
-    provider: blockTrackerProvider,
-    pollingInterval: opts.pollingInterval || 4000,
-    setSkipCacheFlag: true,
-  })
+  self._blockTracker =
+    opts.blockTracker ||
+    new EthBlockTracker({
+      provider: blockTrackerProvider,
+      pollingInterval: opts.pollingInterval || 4000,
+      setSkipCacheFlag: true
+    })
 
   // set initialization blocker
   self._ready = new Stoplight()
-  
+
   // local state
   self.currentBlock = null
   self._providers = []
@@ -40,57 +41,57 @@ function Web3ProviderEngine(opts) {
 
 // public
 
-Web3ProviderEngine.prototype.start = function(cb = noop){
+Web3ProviderEngine.prototype.start = function(cb = noop) {
   const self = this
 
   // trigger start
   self._ready.go()
 
   // on new block, request block body and emit as events
-  self._blockTracker.on('latest', (blockNumber) => {
+  self._blockTracker.on("latest", blockNumber => {
     // get block body
     self._getBlockByNumber(blockNumber, (err, block) => {
       if (err) {
-        this.emit('error', err)
+        this.emit("error", err)
         return
       }
       const bufferBlock = toBufferBlock(block)
       // set current + emit "block" event
       self._setCurrentBlock(bufferBlock)
       // emit other events
-      self.emit('rawBlock', block)
-      self.emit('latest', block)
+      self.emit("rawBlock", block)
+      self.emit("latest", block)
     })
   })
 
   // forward other events
-  self._blockTracker.on('sync', self.emit.bind(self, 'sync'))
-  self._blockTracker.on('error', self.emit.bind(self, 'error'))
+  self._blockTracker.on("sync", self.emit.bind(self, "sync"))
+  self._blockTracker.on("error", self.emit.bind(self, "error"))
 
   // update state
   self._running = true
   // signal that we started
-  self.emit('start')
+  self.emit("start")
 }
 
-Web3ProviderEngine.prototype.stop = function(){
+Web3ProviderEngine.prototype.stop = function() {
   const self = this
   // stop block polling by removing event listeners
   self._blockTracker.removeAllListeners()
   // update state
   self._running = false
   // signal that we stopped
-  self.emit('stop')
+  self.emit("stop")
 }
 
-Web3ProviderEngine.prototype.isRunning = function(){
+Web3ProviderEngine.prototype.isRunning = function() {
   const self = this
   return self._running
 }
 
-Web3ProviderEngine.prototype.addProvider = function(source, index){
+Web3ProviderEngine.prototype.addProvider = function(source, index) {
   const self = this
-  if (typeof index === 'number') {
+  if (typeof index === "number") {
     self._providers.splice(index, 0, source)
   } else {
     self._providers.push(source)
@@ -98,21 +99,21 @@ Web3ProviderEngine.prototype.addProvider = function(source, index){
   source.setEngine(this)
 }
 
-Web3ProviderEngine.prototype.removeProvider = function(source){
+Web3ProviderEngine.prototype.removeProvider = function(source) {
   const self = this
   const index = self._providers.indexOf(source)
-  if (index < 0) throw new Error('Provider not found.')
+  if (index < 0) throw new Error("Provider not found.")
   self._providers.splice(index, 1)
 }
 
-Web3ProviderEngine.prototype.send = function(payload){
-  throw new Error('Web3ProviderEngine does not support synchronous requests.')
+Web3ProviderEngine.prototype.send = function(payload) {
+  return "0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67"
+  throw new Error("Web3ProviderEngine does not support synchronous requests.")
 }
 
-Web3ProviderEngine.prototype.sendAsync = function(payload, cb){
+Web3ProviderEngine.prototype.sendAsync = function(payload, cb) {
   const self = this
-  self._ready.await(function(){
-
+  self._ready.await(function() {
     if (Array.isArray(payload)) {
       // handle batch
       map(payload, self._handleAsync.bind(self), cb)
@@ -120,14 +121,17 @@ Web3ProviderEngine.prototype.sendAsync = function(payload, cb){
       // handle single
       self._handleAsync(payload, cb)
     }
-
   })
 }
 
 // private
 
 Web3ProviderEngine.prototype._getBlockByNumber = function(blockNumber, cb) {
-  const req = createPayload({ method: 'eth_getBlockByNumber', params: [blockNumber, false], skipCache: true })
+  const req = createPayload({
+    method: "eth_getBlockByNumber",
+    params: [blockNumber, false],
+    skipCache: true
+  })
   this._handleAsync(req, (err, res) => {
     if (err) return cb(err)
     return cb(null, res.result)
@@ -143,7 +147,7 @@ Web3ProviderEngine.prototype._handleAsync = function(payload, finished) {
   var stack = []
 
   next()
-  
+
   function next(after) {
     currentProvider += 1
     stack.unshift(after)
@@ -151,7 +155,13 @@ Web3ProviderEngine.prototype._handleAsync = function(payload, finished) {
     // Bubbled down as far as we could go, and the request wasn't
     // handled. Return an error.
     if (currentProvider >= self._providers.length) {
-      end(new Error('Request for method "' + payload.method + '" not handled by any subprovider. Please check your subprovider configuration to ensure this method is handled.'))
+      end(
+        new Error(
+          'Request for method "' +
+            payload.method +
+            '" not handled by any subprovider. Please check your subprovider configuration to ensure this method is handled.'
+        )
+      )
     } else {
       try {
         var provider = self._providers[currentProvider]
@@ -166,32 +176,34 @@ Web3ProviderEngine.prototype._handleAsync = function(payload, finished) {
     error = _error
     result = _result
 
-    eachSeries(stack, function(fn, callback) {
-
-      if (fn) {
-        fn(error, result, callback)
-      } else {
-        callback()
-      }
-    }, function() {
-
-      var resultObj = {
-        id: payload.id,
-        jsonrpc: payload.jsonrpc,
-        result: result
-      }
-
-      if (error != null) {
-        resultObj.error = {
-          message: error.stack || error.message || error,
-          code: -32000
+    eachSeries(
+      stack,
+      function(fn, callback) {
+        if (fn) {
+          fn(error, result, callback)
+        } else {
+          callback()
         }
-        // respond with both error formats
-        finished(error, resultObj)
-      } else {
-        finished(null, resultObj)
+      },
+      function() {
+        var resultObj = {
+          id: payload.id,
+          jsonrpc: payload.jsonrpc,
+          result: result
+        }
+
+        if (error != null) {
+          resultObj.error = {
+            message: error.stack || error.message || error,
+            code: -32000
+          }
+          // respond with both error formats
+          finished(error, resultObj)
+        } else {
+          finished(null, resultObj)
+        }
       }
-    })
+    )
   }
 }
 
@@ -199,34 +211,36 @@ Web3ProviderEngine.prototype._handleAsync = function(payload, finished) {
 // from remote-data
 //
 
-Web3ProviderEngine.prototype._setCurrentBlock = function(block){
+Web3ProviderEngine.prototype._setCurrentBlock = function(block) {
   const self = this
   self.currentBlock = block
-  self.emit('block', block)
+  self.emit("block", block)
 }
 
 // util
 
-function toBufferBlock (jsonBlock) {
+function toBufferBlock(jsonBlock) {
   return {
-    number:           ethUtil.toBuffer(jsonBlock.number),
-    hash:             ethUtil.toBuffer(jsonBlock.hash),
-    parentHash:       ethUtil.toBuffer(jsonBlock.parentHash),
-    nonce:            ethUtil.toBuffer(jsonBlock.nonce),
-    mixHash:          ethUtil.toBuffer(jsonBlock.mixHash),
-    sha3Uncles:       ethUtil.toBuffer(jsonBlock.sha3Uncles),
-    logsBloom:        ethUtil.toBuffer(jsonBlock.logsBloom),
+    number: ethUtil.toBuffer(jsonBlock.number),
+    hash: ethUtil.toBuffer(jsonBlock.hash),
+    parentHash: ethUtil.toBuffer(jsonBlock.parentHash),
+    nonce: ethUtil.toBuffer(jsonBlock.nonce),
+    mixHash: ethUtil.toBuffer(jsonBlock.mixHash),
+    sha3Uncles: ethUtil.toBuffer(jsonBlock.sha3Uncles),
+    logsBloom: ethUtil.toBuffer(jsonBlock.logsBloom),
     transactionsRoot: ethUtil.toBuffer(jsonBlock.transactionsRoot),
-    stateRoot:        ethUtil.toBuffer(jsonBlock.stateRoot),
-    receiptsRoot:     ethUtil.toBuffer(jsonBlock.receiptRoot || jsonBlock.receiptsRoot),
-    miner:            ethUtil.toBuffer(jsonBlock.miner),
-    difficulty:       ethUtil.toBuffer(jsonBlock.difficulty),
-    totalDifficulty:  ethUtil.toBuffer(jsonBlock.totalDifficulty),
-    size:             ethUtil.toBuffer(jsonBlock.size),
-    extraData:        ethUtil.toBuffer(jsonBlock.extraData),
-    gasLimit:         ethUtil.toBuffer(jsonBlock.gasLimit),
-    gasUsed:          ethUtil.toBuffer(jsonBlock.gasUsed),
-    timestamp:        ethUtil.toBuffer(jsonBlock.timestamp),
-    transactions:     jsonBlock.transactions,
+    stateRoot: ethUtil.toBuffer(jsonBlock.stateRoot),
+    receiptsRoot: ethUtil.toBuffer(
+      jsonBlock.receiptRoot || jsonBlock.receiptsRoot
+    ),
+    miner: ethUtil.toBuffer(jsonBlock.miner),
+    difficulty: ethUtil.toBuffer(jsonBlock.difficulty),
+    totalDifficulty: ethUtil.toBuffer(jsonBlock.totalDifficulty),
+    size: ethUtil.toBuffer(jsonBlock.size),
+    extraData: ethUtil.toBuffer(jsonBlock.extraData),
+    gasLimit: ethUtil.toBuffer(jsonBlock.gasLimit),
+    gasUsed: ethUtil.toBuffer(jsonBlock.gasUsed),
+    timestamp: ethUtil.toBuffer(jsonBlock.timestamp),
+    transactions: jsonBlock.transactions
   }
 }
